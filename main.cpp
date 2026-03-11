58a330a6ce5f79d3fc4c35b61e1067cab7d6db1e
Santiago Arias <santiari05@hotmail.com>
2026-03-11 17:34:01 -0500

    Reduce PAUSA_MS from 1000 to 500 in main.cpp to speed up game pacing, enhancing overall gameplay experience.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ const int NUM_TRAMPAS = 5;
 const int NUM_QUESOS = 1;
 const int POS_INICIAL_RATON_F = 0;
 const int POS_INICIAL_RATON_C = 0;
-const int PAUSA_MS = 1000;
+const int PAUSA_MS = 500;
 
 const int ESTADO_EN_CURSO = 0;
 const int ESTADO_VICTORIA = 1;
