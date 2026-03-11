73e95600ae9a293f407a6f0565f446abfe7310b1
Santiago Arias <santiari05@hotmail.com>
2026-03-11 15:27:56 -0500

    Update main.cpp to define constants for game parameters and add temp.cpp to .gitignore

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World!";
-}
+const int FILAS = 12;
+const int COLUMNAS = 10;
+const int NUM_GATOS = 5;
+const int NUM_TRAMPAS = 5;
+const int NUM_QUESOS = 1;
+const int POS_INICIAL_RATON_F = 0;
+const int POS_INICIAL_RATON_C = 0;
+const int PAUSA_MS = 1000;
